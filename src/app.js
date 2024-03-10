fb6293c8d3c10235025c46ea0865ce1276006b86
Klaudia Pałubska <klaudiapalubska@gmail.com>
2024-03-10 16:13:14 +0100

    index

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 });
 
-server.listen(8080, '127.0.0.1', () => {
+server.listen(8000, '127.0.0.1', () => {
   console.log('8080 port');
 });
